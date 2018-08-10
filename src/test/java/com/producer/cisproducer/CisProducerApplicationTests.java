552a2df568d4baf414ba210e9c4332ba3c3b1acc
Ocean <dvsocean@icloud.com>
2018-08-10 06:58:12 -0700

    refact demo from scratch

---
+++ src/test/java/com/producer/cisproducer/CisProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.producer.cisproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CisProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
